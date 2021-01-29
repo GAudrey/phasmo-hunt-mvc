4cb5c23d8df1b3c209605bea24ae2dab1bd757d9
Audrey <73021969+GAudrey@users.noreply.github.com>
2021-01-29 15:20:10 +0100

    whiteboard db form displayed

--- components/functions.php
+++ components/functions.php
@@ -1,7 +1,7 @@
 <?php
     function dbConnect(){
         include('db.php');
-        return new PDO("mysql:host=localhost;dbname=phasmophobia;charset=utf8", $phpUser, $phpPasswd,
+        return new PDO("mysql:host=localhost;dbname=phasmophobia;charset=utf8", $phpUser, $phpPwd,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -12,9 +12,27 @@
     function objectives(){
         $db = dbConnect();
 
-        $query = $db->prepare("SELECT `objective` FROM `t_objectives`");
+        $query = $db->prepare("SELECT `short_objective`, `objective` FROM `t_objectives`");
         $query->execute();
 
-        return $query->fetch();
+        return $query->fetchAll();
+    }
+
+    function firstname(){
+        $db = dbConnect();
+
+        $query = $db->prepare("SELECT `firstname` FROM `t_firstname`");
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    function lastname(){
+        $db = dbConnect();
+
+        $query = $db->prepare("SELECT `lastname` FROM `t_lastname`");
+        $query->execute();
+
+        return $query->fetchAll();
     }
 ?>
--- index.php
+++ index.php
@@ -15,26 +15,62 @@
         <!-- WHITEBOARD -->
         <section id="whiteboard">
             <form method="post" action="">
+                <!-- OBJECTIVES -->
                 <div>
-                    <?php
-                        objective();
+                    <?php 
+                        $objectivesList = objectives();
+
+                        foreach($objectivesList as $secObj):
+
+                        $objString = $secObj['objective'];
+                        $objShort = $secObj['short_objective'];
                     ?>
+                    <div>
+                        <input type="checkbox" id="<?php echo $objShort; ?>" name="<?php echo $objShort; ?>">
+                        <label for="<?php echo $objShort; ?>"><?php echo $objString; ?></label>
+                    </div>
+                    <?php endforeach; ?>
                 </div>
+                <!-- FIRST NAMES -->
+                <div>
+                    <label for="ghost_firstname">What's you ghost first name?</label>
+                    <select name="firstname" id="ghost_firstname">
+                        <option>- Select a first name</option>
+                        <?php
+                            $firstnList = firstname();
 
-                <!-- <label for="ghost_firstname">What's you ghost first name?</label>
-                <select name="firstname" id="ghost_firstname" value="">
-                    <option>- Select a first name</option>
+                            foreach($firstnList as $frtn):
 
-                </select>
-                <label for="ghost_lastname">What's you ghost last name?</label>
-                <select name="lastname" id="ghost_lastname" value="">
-                    <option>- Select a last name</option>
-                    
-                </select>
-                <input type="radio" id="talk_solo" name="talk" value="talk_solo">
-                <label for="talk_solo">Huey</label>
-                <input type="radio" id="talk_multi" name="talk" value="talk_multi">
-                <label for="talk_multi">Huey</label> -->
+                            $frtnItem = $frtn['firstname'];
+                        ?>
+                        <option value="<?php echo $frtnItem; ?>"><?php echo $frtnItem; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <!-- LAST NAMES -->
+                <div>
+                    <label for="ghost_lastname">What's you ghost last name?</label>
+                    <select name="lastname" id="ghost_lastname">
+                        <option>- Select a last name</option>
+                        <?php
+                            $lastnList = lastname();
+
+                            foreach($lastnList as $lstn):
+
+                            $lstnItem = $lstn['lastname'];
+                        ?>
+                        <option value="<?php echo $lstnItem; ?>"><?php echo $lstnItem; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <!-- TALK TO -->
+                <div>
+                    <p>Respond to:</p>
+                    <input type="radio" id="talk_solo" name="talk" value="talk_solo">
+                    <label for="talk_solo">people who are alone</label>
+                    <input type="radio" id="talk_multi" name="talk" value="talk_multi">
+                    <label for="talk_multi">everyone</label>
+                </div>
             </form>
         </section>
 
