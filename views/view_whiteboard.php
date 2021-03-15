9f939270c896d215bf3c017fbfb7d7e4c40fead2
GAudrey <gilmantaudrey.ag@gmail.com>
2021-03-15 16:42:41 +0100

    minor changes for talk display

--- views/view_whiteboard.php
+++ views/view_whiteboard.php
@@ -6,11 +6,11 @@
     <p>Objective 2: <?= $whiteboard->getObj1()[0]; ?>.</p>
     <p>Objective 3: <?= $whiteboard->getObj2()[0]; ?>.</p>
     <p>Objective 4: <?= $whiteboard->getObj3()[0]; ?>.</p>
-    <p>I've done some investigating for you. Looks like the ghost's name is <?= $whiteboard->getFirstn() . ' ' . $whiteboard->getLastn(); ?>. This Ghost also seems to only respond to 
+    <p>I've done some investigating for you. Looks like the ghost's name is <?= $whiteboard->getFirstn() . ' ' . $whiteboard->getLastn(); ?>. This Ghost also seems to 
     <?php if($whiteboard->getResp() == 'talk_solo'): ?>
-        people who are alone.
+        only respond to people who are alone.
     <?php elseif($whiteboard->getResp() == 'talk_multi'): ?>
-        everyone.
+        respond to everyone.
     <?php endif; ?>
     You should be albe to use it's name to anger it and get some paranormal activity. Make sure to refer to your Journal and write down any evidence you find.</p>
 </section>
